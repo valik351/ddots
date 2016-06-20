a0643a99ef7d239aaf17436bc187a38889cc32f6
valik351 <av.val.100@gmail.com>
2016-06-20 16:54:25 -0700

    list of servers

--- resources/views/admin/testing_servers/list.blade.php
+++ resources/views/admin/testing_servers/list.blade.php
@@ -1,17 +1,32 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="container">
+                    <h2>Testing Servers</h2>
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Server name</th>
+                            <th>Auth key</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($servers as $server)
+                            <tr>
+                                <td>{{ $server->id }}</td>
+                                <td>{{ $server->name }}</td>
+                                <td>{{ $server->auth_key }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                    {{ $servers->links() }}
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
