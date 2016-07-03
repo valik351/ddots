5e0f925545e8c69160683b8a07f44a11267eff3d
valik351 <av.val.100@gmail.com>
2016-07-03 07:24:00 -0700

    fix subdomains

--- app/Subdomain.php
+++ app/Subdomain.php
@@ -8,9 +8,11 @@ class Subdomain extends Model
 {
     const DEFAULT_SUB_DOMAIN = 'labs';
     public static function currentSubdomainName() {
-        $subdomain = self::where('name', explode('.', \Request::getHost())[0])
-            ->get()
-            ->first();
+        if(\Schema::hasTable('subdomains')) {
+            $subdomain = self::where('name', explode('.', \Request::getHost())[0])
+                ->get()
+                ->first();
+        }
         if(empty($subdomain)) {
             return self::DEFAULT_SUB_DOMAIN;
         }
