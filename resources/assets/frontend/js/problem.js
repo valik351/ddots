26a4249453c0ca04f1260fbd3accb679ef2a3186
valik351 <av.val.100@gmail.com>
2016-07-20 11:38:49 -0700

    fix ace editor

--- resources/assets/frontend/js/problem.js
+++ resources/assets/frontend/js/problem.js
@@ -4,6 +4,9 @@
             if($('#editor').length) {
                 var editor = ace.edit('editor');
                 editor.setTheme("ace/theme/monokai");
+                editor.getSession().setTabSize(4);
+                editor.getSession().setUseSoftTabs(true);
+                editor.getSession().setUseWrapMode(true);
                 editor.getSession().setMode("ace/mode/javascript");
             }
             $('[data-submit-solution]').submit(function (e) {
