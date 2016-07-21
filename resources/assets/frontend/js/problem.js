bdea95e9be35b609772530b91a0b770a79b680db
qazwsxedcrfvtgb1111 <tttdima60@gmail.com>
2016-07-21 19:18:23 +0300

    ace ctrl+s fix

--- resources/assets/frontend/js/problem.js
+++ resources/assets/frontend/js/problem.js
@@ -1,34 +1,40 @@
-(
-    function($, window, document) {
-        $(document).ready(function() {
-            if($('#editor').length) {
+(function ($, window, document) {
+    $(document).ready(function () {
+        if ($('#editor').length) {
 
-                var editor = ace.edit('editor');
-                editor.setOptions({
-                    enableBasicAutocompletion: true,
-                    enableLiveAutocompletion: true,
-                    showInvisibles: true,
-                    tabSize: 4,
-                    wrap: true,
-                    useSoftTabs: true,
-                    theme: "ace/theme/monokai"
-                });
-                
-                if($('[data-solution]').length) {
-                    editor.getSession().setMode("ace/mode/" + $('[data-ace-mode]').data('ace-mode'));
-                    editor.setReadOnly(true);
-                }
+            var editor = ace.edit('editor');
+            editor.setOptions({
+                enableBasicAutocompletion: true,
+                enableLiveAutocompletion: true,
+                showInvisibles: true,
+                tabSize: 4,
+                wrap: true,
+                useSoftTabs: true,
+                theme: "ace/theme/monokai"
+            });
 
-                $('[data-programming-languages]').change(function(){
-                    editor.getSession().setMode("ace/mode/" + $('[data-ace-mode]:selected').data('ace-mode'));
-                });
+            editor.commands.addCommand({
+                name: "save",
+                bindKey: {win: "Ctrl-s", mac: "Command-s"},
+                exec: function(){},
+            });
+
+
+            if ($('[data-solution]').length) {
+                editor.getSession().setMode("ace/mode/" + $('[data-ace-mode]').data('ace-mode'));
+                editor.setReadOnly(true);
             }
 
-            $('[data-submit-solution]').submit(function (e) {
-                var code = editor.getValue();
-                if(code.length) {
-                    $('input[name=solution_code]').val(code)
-                }
-            })
-        });
-    })(jQuery, window, document);
+            $('[data-programming-languages]').change(function () {
+                editor.getSession().setMode("ace/mode/" + $('[data-ace-mode]:selected').data('ace-mode'));
+            });
+        }
+
+        $('[data-submit-solution]').submit(function (e) {
+            var code = editor.getValue();
+            if (code.length) {
+                $('input[name=solution_code]').val(code)
+            }
+        })
+    });
+})(jQuery, window, document);
