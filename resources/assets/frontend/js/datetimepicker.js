06b260d33d6b54e88c6faa68f4f91e9b27429049
qazwsxedcrfvtgb1111 <tttdima60@gmail.com>
2016-07-22 18:30:42 +0300

    js fix

--- resources/assets/frontend/js/datetimepicker.js
+++ resources/assets/frontend/js/datetimepicker.js
@@ -2,7 +2,7 @@
     function($, window, document) {
         $(document).ready(function() {
             $('[data-datepicker]').datetimepicker({ format: 'D-MM-Y', minDate: '1920-01-01', maxDate: moment()});
-            $('[data-start-datepicker]').datetimepicker({format: 'Y-MM-DD H:mm:ss'})
-            $('[data-end-datepicker]').datetimepicker({format: 'Y-MM-DD H:mm:ss'})
+            $('[data-start-datepicker]').datetimepicker({format: 'Y-MM-DD H:mm:ss'});
+            $('[data-end-datepicker]').datetimepicker({format: 'Y-MM-DD H:mm:ss'});
         });
     })(jQuery, window, document);
