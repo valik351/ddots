c4b98c44f1e46637ba823fc1d19401069db01165
qazwsxedcrfvtgb1111 <tttdima60@gmail.com>
2016-07-22 17:08:07 +0300

    fix

--- app/Http/Controllers/ContestController.php
+++ app/Http/Controllers/ContestController.php
@@ -61,48 +61,49 @@ class ContestController extends Controller
     public function showForm(Request $request, $id = null)
     {
         $contest = ($id ? Contest::findOrFail($id) : new Contest());
-            $participants = collect();
-            $students = Auth::user()->students()->get();
-            if ($id) {
-                $title = 'Edit Contest';
-                if (Session::get('errors')) {
-                    foreach ($students as $student) {
-                        if (in_array($student->id, (array)old('participants'))) {
-                            $participants->push($student);
-                        }
+        $participants = collect();
+        $students = Auth::user()->students()->get();
+        if ($id) {
+            $title = 'Edit Contest';
+            if (Session::get('errors')) {
+                foreach ($students as $student) {
+                    if (in_array($student->id, (array)old('participants'))) {
+                        $participants->push($student);
                     }
-
-                    $included_problems = collect();
-                    $problems = Problem::orderBy('name', 'desc')->get();
-
-                    foreach ($problems as $problem) {
-                        if (in_array($problem->id, (array)old('problems'))) {
-                            $included_problems->push($problem);
-                        }
-                    }
-                    $unincluded_problems = $problems->diff($included_problems);
-
-                } else {
-                    $participants = $contest->users()->user()->get();
-                    $included_problems = $contest->problems;
-                    $unincluded_problems = Problem::orderBy('name', 'desc')->get()->diff($included_problems);
                 }
-                $students = $students->diff($participants);
+
+                $included_problems = collect();
+                $problems = Problem::orderBy('name', 'desc')->get();
+
+                foreach ($problems as $problem) {
+                    if (in_array($problem->id, (array)old('problems'))) {
+                        $included_problems->push($problem);
+                    }
+                }
+                $unincluded_problems = $problems->diff($included_problems);
+
             } else {
-                $title = 'Create Contest';
+                $participants = $contest->users()->user()->get();
+                $included_problems = $contest->problems;
+                $unincluded_problems = Problem::orderBy('name', 'desc')->get()->diff($included_problems);
             }
+            $students = $students->diff($participants);
+        } else {
+            $title = 'Create Contest';
+            $unincluded_problems = Problem::orderBy('name', 'desc')->get();
+            $included_problems = collect();
+        }
 
 
-
-            return view('contests.form')->with([
-                'contest' => $contest,
-                'title' => $title,
-                'students' => $students,
-                'participants' => $participants,
-                'programming_languages' => ProgrammingLanguage::orderBy('name', 'desc')->get(),
-                'included_problems' => $included_problems,
-                'unincluded_problems' => $unincluded_problems
-            ]);
+        return view('contests.form')->with([
+            'contest' => $contest,
+            'title' => $title,
+            'students' => $students,
+            'participants' => $participants,
+            'programming_languages' => ProgrammingLanguage::orderBy('name', 'desc')->get(),
+            'included_problems' => $included_problems,
+            'unincluded_problems' => $unincluded_problems
+        ]);
     }
 
     /**
@@ -116,46 +117,47 @@ class ContestController extends Controller
     public function edit(Request $request, $id = null)
     {
         $contest = (!$id ?: Contest::findOrFail($id));
-            $fillData = [
-                'name' => $request->get('name'),
-                'description' => $request->get('description'),
-                'start_date' => $request->get('start_date'),
-                'end_date' => $request->get('end_date'),
-                'user_id' => Auth::user()->id,
-                'is_active' => $request->get('is_active'),
-                'is_standings_active' => $request->get('is_standings_active'),
-                'show_max' => $request->get('show_max'),
-                'labs' => $request->get('labs'),
-            ];
+        $fillData = [
+            'name' => $request->get('name'),
+            'description' => $request->get('description'),
+            'start_date' => $request->get('start_date'),
+            'end_date' => $request->get('end_date'),
+            'user_id' => Auth::user()->id,
+            'is_active' => $request->get('is_active'),
+            'is_standings_active' => $request->get('is_standings_active'),
+            'show_max' => $request->get('show_max'),
+            'labs' => $request->get('labs'),
+        ];
 
-            $this->validate($request, Contest::getValidationRules(), ['programming_languages.required' => 'At least one language must be selected.']);
+        $this->validate($request, Contest::getValidationRules(), ['programming_languages.required' => 'At least one language must be selected.']);
 
 
-            if ($id) {
-                $contest->fill($fillData);
-            } else {
-                $contest = Contest::create($fillData);
-            }
+        if ($id) {
+            $contest->fill($fillData);
+        } else {
+            $contest = Contest::create($fillData);
+        }
 
-            $contest->programming_languages()->sync($request->get('programming_languages') ? $request->get('programming_languages') : []);
+        $contest->programming_languages()->sync($request->get('programming_languages') ? $request->get('programming_languages') : []);
 
-            $contest->problems()->sync($request->get('problems') ? array_combine($request->get('problems'), array_map(function ($a) {
-                return ['max_points' => $a];
-            }, $request->get('problem_points'))) : []);
+        $contest->problems()->sync($request->get('problems') ? array_combine($request->get('problems'), array_map(function ($a) {
+            return ['max_points' => $a];
+        }, $request->get('problem_points'))) : []);
 
-            $contest->users()->sync((array)$request->get('participants'));
+        $contest->users()->sync((array)$request->get('participants'));
 
-            $contest->save();
+        $contest->save();
 
-            \Session::flash('alert-success', 'The contest was successfully saved');
+        \Session::flash('alert-success', 'The contest was successfully saved');
+        return redirect()->route('frontend::contests::list');
     }
 
     public function hide(Request $request, $id)
     {
 
         $contest = Contest::findOrFail($id);
-            $contest->hide();
-            $contest->save();
+        $contest->hide();
+        $contest->save();
         return redirect()->route('frontend::contests::list');
     }
 
