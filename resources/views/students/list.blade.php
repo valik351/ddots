4585517388160099def3c2ab839098e61c1e2c9d
andre <remezandriy@gmail.com>
2016-09-01 01:14:21 +0300

    added a card to a students' list

--- resources/views/students/list.blade.php
+++ resources/views/students/list.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
 
         <div class="row">
-            <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="col-md-12 col-sm-12 col-xs-12 card">
                 <div class="x_panel">
                     <div class="x_title">
                         <h2>Students</h2>
