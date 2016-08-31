51a563b1b90f76faea7e9f07689512377acd48ac
andre <remezandriy@gmail.com>
2016-09-01 00:24:00 +0300

    добавил карточку

--- resources/views/messages/new.blade.php
+++ resources/views/messages/new.blade.php
@@ -1,30 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
-                    <div class="x_title">
-                        <a href="{{ route('frontend::messages::list') }}">Back to dialogs</a>
-                        <div class="clearfix"></div>
-                    </div>
 
-                    <form method="post">
-                        <select name="user_id">
-                            @foreach($users as $user)
-                                <option value="{{ $user->id }}">{{ $user->name }}</option>
-                            @endforeach
-                        </select>
-                        {{ csrf_field() }}
-                        <textarea name="text"></textarea>
-                        @if($errors->has('text'))
-                            <span><strong>{{ $errors->first('text') }}</strong></span>
-                        @endif
-                        <input type="submit">
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+
+<a href="{{ route('frontend::messages::list') }}">Back to dialogs</a>
+<div class="card">
+<form method="post">
+    <select name="user_id">
+        @foreach($users as $user)
+            <option value="{{ $user->id }}">{{ $user->name }}</option>
+        @endforeach
+    </select>
+    {{ csrf_field() }}
+    <textarea name="text"></textarea>
+    @if($errors->has('text'))
+        <span><strong>{{ $errors->first('text') }}</strong></span>
+    @endif
+    <input type="submit">
+</form>
+</div>
+
+
+
+
+
+
+
 @endsection
