62bdbd06cece9bbc8a851fcfdbea1c0c20270606
valik351 <av.val.100@gmail.com>
2016-10-24 22:23:40 +0300

    navbar, siderbar, header rebuild

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -1,23 +1,59 @@
-<ul class="nav navbar-nav navbar-right">
-    @if(Auth::guest())
-        <li {!! Request::is('login') ? 'class="active"' : '' !!}>
-            <a href="{{ url('/login') }}">
-                <i class="ti-angle-double-up"></i>
-                <p>Login</p>
+
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+
+            <!-- Collapsed Hamburger -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ action('HomeController@index') }}">
+                {{ App\Subdomain::currentSubdomain()->fullname }}
             </a>
-        </li>
-        <li {!! Request::is('register') ? 'class="active"' : '' !!}>
-            <a href="{{ url('/register') }}">
-                <i class="ti-angle-double-right"></i>
-                <p>Register</p>
-            </a>
-        </li>
-    @else
-        <li {!! Request::is('logout') ? 'class="active"' : '' !!}>
-            <a href="{{ url('/logout') }}">
-                <i class="ti-angle-double-down"></i>
-                <p>Logout</p>
-            </a>
-        </li>
-    @endif
-</ul>
+        </div>
+
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Left Side Of Navbar -->
+            <ul class="nav navbar-nav">
+                <li><a href="{{ url('/teachers') }}">Teachers</a></li>
+                @if (Auth::check())
+                    <li><a href="{{ route('frontend::user::profile', ['id' => Auth::user()->id]) }}">Profile</a></li>
+                @endif
+
+                @if (Auth::check() && Auth::user()->hasRole(App\User::ROLE_TEACHER))
+                    <li><a href="{{ url('/groups') }}">Groups</a></li>
+                    <li><a href="{{ url('/students') }}">Students</a></li>
+                @endif
+                @if(Auth::check())
+                    <li><a href="{{ url('/contests') }}">Contests</a></li>
+                    <li><a href="{{ url('/messaging') }}">Messaging</a></li>
+                @endif
+            </ul>
+
+
+            <!-- Right Side Of Navbar -->
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Authentication Links -->
+                @if (Auth::guest())
+                    <li><a href="{{ url('/login') }}">Login</a></li>
+                    <li><a href="{{ url('/register') }}">Register</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                        </ul>
+                    </li>
+                @endif
+            </ul>
+        </div>
+    </div>
+</nav>
