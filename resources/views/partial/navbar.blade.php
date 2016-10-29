de63fc184911a2f037bd36ca355699a8e86035e2
valik351 <av.val.100@gmail.com>
2016-10-29 22:10:27 +0300

    finish navbar

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -104,19 +104,16 @@
 
         <nav>
             <button class="navbar-toggler float-xs-right hidden-sm-up" type="button" data-toggle="collapse" data-target="#bd-main-nav" aria-controls="bd-main-nav" aria-expanded="false" aria-label="Toggle navigation"></button>
-            <a class="navbar-brand" href="{{ action('HomeController@index') }}">
-                {{ App\Subdomain::currentSubdomain()->fullname }}
-            </a>
-            <div class="collapse navbar-toggleable-xs" id="bd-main-nav">
 
+            <div class="collapse navbar-toggleable-xs" id="bd-main-nav">
                 <a class="navbar-brand" href="{{ action('HomeController@index') }}">
                     {{ App\Subdomain::currentSubdomain()->fullname }}
                 </a>
-                <ul class="nav navbar-nav">
+                <ul class="nav navbar-nav ">
                     <li class="nav-item {{ Request::is('teachers') ? 'active' : '' }}"><a class="nav-item nav-link" href="{{ url('/teachers') }}">Teachers</a></li>
 
                     @if (Auth::check())
-                        <li class="nav-item {{ Request::is(route('frontend::user::profile', ['id' => Auth::user()->id])) ? 'active' : '' }}"><a class="nav-item nav-link" href="{{ route('frontend::user::profile', ['id' => Auth::user()->id]) }}">Profile</a></li>
+                        <li class="nav-item {{ Request::fullUrl() == route('frontend::user::profile', ['id' => Auth::user()->id]) ? 'active' : '' }}"><a class="nav-item nav-link" href="{{ route('frontend::user::profile', ['id' => Auth::user()->id]) }}">Profile</a></li>
                     @endif
 
                     @if (Auth::check() && Auth::user()->hasRole(App\User::ROLE_TEACHER))
@@ -126,6 +123,13 @@
                     @if(Auth::check())
                         <li class="nav-item {{ Request::is('contests') ? 'active' : '' }}"><a class="nav-item nav-link" href="{{ url('/contests') }}">Contests</a></li>
                         <li class="nav-item {{ Request::is('messaging') ? 'active' : '' }}"><a class="nav-item nav-link" href="{{ url('/messaging') }}">Messaging</a></li>
+                    @endif
+
+                    @if (Auth::check())
+                        <li class="nav-item float-md-right"><a class="nav-item nav-link" href="{{ url('/logout') }}"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a></li>
+                    @endif
+                    @if (!Auth::check())
+                        <li class="nav-item float-md-right"><a class="nav-item nav-link" href="{{ url('/register') }}"><i class="fa fa-sign-in" aria-hidden="true"></i> Register</a></li>
                     @endif
                 </ul>
             </div>
