8649b0ce4ef0f95c24541af2a07091bde1a81906
valik351 <av.val.100@gmail.com>
2016-10-30 15:03:32 +0200

    profile page

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -4,9 +4,10 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 offset-md-1 col-lg-8 offset-lg-2">
-                <div class="card  text-xs-center">
-                    <a href="{{ route('frontend::user::profile', ['id' => $user->id]) }}"><img class="card-img-top teacher-avatar" src="{{ $user->avatar }}" alt="Card image cap"></a>
+                <div class="card text-xs-center">
+                    <div class="card-header text-xs-left">Profile</div>
                     <div class="card-block">
+                        <a href="{{ route('frontend::user::profile', ['id' => $user->id]) }}"><img class="card-img-top teacher-avatar" src="{{ $user->avatar }}" alt="Card image cap"></a>
                         <h4>{{ $user->name }}<br>
                             <a href="{{ route('frontend::user::profile', ['id' => $user->id]) }}"><small>{{ $user->nickname }}</small></a>
                         </h4>
@@ -70,43 +71,33 @@
 
                 @if($user->hasRole(\App\User::ROLE_USER) && $user->teachers()->count())
                     <div class="card">
-                        <div class="header">
-                            <h4 class="title">Teachers</h4>
-                        </div>
-                        <div class="content">
-                            <ul class="list-unstyled team-members">
-
+                        <div class="card-header">Teachers</div>
+                        <div class="card-block">
+                            <div class="row">
                                 @foreach($user->teachers as $teacher)
-                                    <li>
-                                        <div class="row">
-                                            <div class="col-xs-3">
+                                    <div class="col-md-4">
+                                        <div class="card text-xs-center">
+                                            <a href="{{ action('UserController@index', ['id' => $teacher->id]) }}">
+                                                <img class="card-img-top teacher-avatar" src="{{ $teacher->avatar }}" alt="Card image cap">
+                                            </a>
+                                            <div class="card-block">
                                                 <a href="{{ action('UserController@index', ['id' => $teacher->id]) }}">
-                                                    <div class="avatar">
-                                                        <img src="{{ $teacher->avatar }}" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-                                                    </div>
+                                                    <h4 class="card-title">{{ $teacher->name }}</h4>
+                                                    <p class="card-text"><small class="text-muted">{{ $teacher->nickname }}</small></p>
                                                 </a>
-                                            </div>
-                                            <div class="col-xs-6">
-
-                                                <a href="{{ action('UserController@index', ['id' => $teacher->id]) }}">
-                                                    {{ $teacher->name }}
-                                                    <br>
-                                                    <span class="text-muted"><small>{{ $teacher->nickname }}</small></span>
-                                                </a>
-                                            </div>
-
-                                            <div class="col-xs-3 text-right">
-                                                @if($teacher->vk_link)
-                                                    <a href="{{ $teacher->vk_link }}"><i class="dots-vk-icon"></i></a>
-                                                @endif
-                                                @if($teacher->fb_link)
-                                                    <a href="{{ $teacher->fb_link }}"><i class="dots-facebook-icon"></i></a>
-                                                @endif
+                                                <div class="card-text">
+                                                    @if($teacher->vk_link)
+                                                        <a class="btn btn-social-icon btn-vk" href="{{ $teacher->vk_link }}"><span class="fa fa-vk"></span></a>
+                                                    @endif
+                                                    @if($teacher->fb_link)
+                                                        <a class="btn btn-social-icon btn-facebook" href="{{ $teacher->fb_link }}"><span class="fa fa-facebook"></span></a>
+                                                    @endif
+                                                </div>
                                             </div>
                                         </div>
-                                    </li>
+                                    </div>
                                 @endforeach
-                            </ul>
+                            </div>
                         </div>
                     </div>
 
