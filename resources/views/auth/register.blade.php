acd990104712b9594aa870d01b7a78da21490f32
valik351 <av.val.100@gmail.com>
2016-10-30 21:01:58 +0200

    add register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,17 +5,17 @@
 
         <div class="row">
             <div class="col-sm-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Register</div>
-                    <div class="panel-body">
-                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+                <div class="card">
+                    <div class="card-header">Register</div>
+                    <div class="card-block">
+                        <form class="form" role="form" method="POST" action="{{ url('/register') }}">
                             {{ csrf_field() }}
 
-                            <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
+                            <div class="form-group row {{ $errors->has('name') ? ' has-danger' : '' }}">
 
-                                <label for="name" class="col-md-4 control-label">Name</label>
+                                <label for="name" class="col-md-4 control-label">Name*</label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-8">
                                     <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}">
                                     @if ($errors->has('name'))
                                         <span class="form-control-feedback">
@@ -25,11 +25,11 @@
                                 </div>
                             </div>
 
-                            <div class="form-group{{ $errors->has('nickname') ? ' has-danger' : '' }}">
+                            <div class="form-group row {{ $errors->has('nickname') ? ' has-danger' : '' }}">
 
-                                <label for="nickname" class="col-md-4 control-label">Nickname</label>
+                                <label for="nickname" class="col-md-4 control-label">Nickname*</label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-8">
                                     <input id="nickname" type="text" class="form-control" name="nickname" value="{{ old('nickname') }}">
 
                                     @if ($errors->has('nickname'))
@@ -41,11 +41,11 @@
                             </div>
 
 
-                            <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
+                            <div class="form-group row {{ $errors->has('password') ? ' has-danger' : '' }}">
 
-                                <label for="password" class="col-md-4 control-label">Password</label>
+                                <label for="password" class="col-md-4 control-label">Password*</label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-8">
                                     <input id="password" type="password" class="form-control" name="password" value="{{ old('password') }}">
 
                                     @if ($errors->has('password'))
@@ -56,11 +56,11 @@
                                 </div>
                             </div>
 
-                            <div class="form-group{{ $errors->has('password_confirmation') ? ' has-danger' : '' }}">
+                            <div class="form-group row {{ $errors->has('password_confirmation') ? ' has-danger' : '' }}">
 
-                                <label for="password_confirmation" class="col-md-4 control-label">Confirm Password</label>
+                                <label for="password_confirmation" class="col-md-4 control-label">Confirm Password*</label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-8">
                                     <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" value="{{ old('password_confirmation') }}">
 
                                     @if ($errors->has('password_confirmation'))
@@ -71,8 +71,8 @@
                                 </div>
                             </div>
 
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
+                            <div class="form-group row ">
+                                <div class="col-md-8 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
                                         <i class="fa fa-btn fa-user"></i> Register
                                     </button>
@@ -81,9 +81,6 @@
                                     <a class="btn btn-social-icon btn-facebook" href="{{ route('social::redirect', ['provider' => 'facebook']) }}"><span class="fa fa-facebook"></span></a>
 
                                 </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4"></div>
                             </div>
                         </form>
                     </div>
