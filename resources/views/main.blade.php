8a067a6f5777ed82e7e1e594da22e50a72a6bc3d
valik351 <av.val.100@gmail.com>
2016-10-30 13:18:31 +0200

    main page finish

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,154 +1,207 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="content dots-domain-content">
-        <div class="container-fluid">
+    <div class="container">
 
-            <!-- logo and title -->
-            <div class="row">
-                <div class="col-lg-3 col-sm-6">
-                    <div class="dots-logo">
-                        <a href="http://dots.org.ua/dots/"></a>
-                    </div>
-                </div>
-                <div class="col-lg-9 col-sm-6 dots-tagline">
-                    <h1>
-                        <p>
-                            С 16 февраля 2016 тестирующая система DOTS доступна для преподавателей информатики Харькова
-                            и Харьковской области. Теперь все, кто преподаёт программирование, сможет зарегистрировать в
-                            системе своих учеников, объединять их в группы и проводить практикумы по программированию с
-                            использованием тестирующей системы. </p>
-                    </h1>
-                </div>
+        <!-- logo and title -->
+        <div class="row">
+            <div class="col-lg-2 col-sm-6">
+                <a class="navbar-brand" href="{{ action('HomeController@index') }}">
+                    <img src="{{ asset('frontend-bundle/media/dots.png') }}">
+                </a>
             </div>
-
-            <!-- description -->
-            <div class="row">
-                <div class="col-lg-12 col-sm-12 dots-wrap-content">
-                    <h2>Что такое dots?</h2>
-                    <p class="dots-card" style="text-align: justify;">
-                        <b>Dots</b> - это тестирующая система для dots.org.ua, написаная на Python, sh и C и интенсивно
-                        использующая все возможности Docker контейнеров и Cgroup подсистемы ядра, запуская компиляторы и
-                        тестируя пользовательские решения в отдельных Docker контейнерах. Такой подход делает DDots
-                        максимально гибкой и модульной системой. <br> <br>
-
-                        Один DDots обрабатывает одно решение в единицу времени и предзагружает два решения в очередь для
-                        уменьшения задержек, связанных с сетью. Таким образом, для нагрузки нескольких ядер
-                        рекомендуется запускать N контейнеров DDots (это делается автоматически, Makefile автоматически
-                        определяет количество доступных ядер и использует все возможности). <br><br>
-                    </p>
-                </div>
+            <div class="col-lg-10 col-sm-6">
+                <h4>
+                    <p class="text-justify">
+                        С 16 февраля 2016 тестирующая система DOTS доступна для преподавателей информатики Харькова
+                        и Харьковской области. Теперь все, кто преподаёт программирование, сможет зарегистрировать в
+                        системе своих учеников, объединять их в группы и проводить практикумы по программированию с
+                        использованием тестирующей системы. </p>
+                </h4>
             </div>
-
-            <!-- news -->
-            <div class="row">
-                <div class="col-lg-12 col-sm-12 dots-wrap-content">
-                    <h2>Новости</h2>
-                    <div class="dots-card">
-
-                        <div class="dots-news-wrap">
-                            <p>Текст последней новости</p>
-                            <a href="#">Подробнее</a>
-                        </div>
-
-                        <a href="#" class="dots-full-content-link"><i class="ti-arrow-circle-down"></i>Все
-                            новости</a>
-                    </div>
-                </div>
-            </div>
-
-            <!-- practical -->
-            <div class="row">
-                <div class="col-lg-12 col-sm-12 dots-wrap-content">
-                    <h2>Практикум по программированию</h2>
-                    <div class="dots-card">
-                        <div class="content table-responsive table-full-width">
-                            <table class="table">
-                                <tbody>
-                                @foreach(\App\Subdomain::get() as $subdomain)
-                                    <tr>
-                                        <td class="dots-tb-cont-logo"><a href="{{ $subdomain->getUrl() }}"><img
-                                                        src="{{ $subdomain->image }}"
-                                                        alt="subdomain-logo"/></a></td>
-                                        <td class="dots-tb-cont-name"><a href="{{ $subdomain->getUrl() }}">{{ $subdomain->title }}</a></td>
-                                        <td class="dots-tb-cont-description">{{ $subdomain->description }}</td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-
-                        <a href="#" class="dots-full-content-link"><i class="ti-arrow-circle-down"></i>Все
-                            практикумы</a>
-                    </div>
-                </div>
-            </div>
-
-            <!-- sponsors -->
-            <div class="row">
-                <div class="col-lg-12 col-sm-12 dots-wrap-content">
-                    <h2>Проект поддержали</h2>
-                    <div class="dots-card">
-                        <div class="content table-responsive table-full-width">
-                            <table class="table">
-                                <tbody>
-                                @foreach(\App\Sponsor::main()->get() as $sponsor)
-                                    <tr>
-                                        <td class="dots-tb-cont-logo"><a href="{{ $sponsor->link }}"><img
-                                                        src="{{ $sponsor->image }}" alt="sponsor-logo"/></a></td>
-                                        <td class="dots-tb-cont-name"><a
-                                                    href="{{ $sponsor->link }}">{{ $sponsor->name }}</a></td>
-                                        <td class="dots-tb-cont-description">{{ $sponsor->description }}</td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-
-                        <a href="#" class="dots-full-content-link"><i class="ti-arrow-circle-down"></i>Все спонсоры</a>
-                    </div>
-                </div>
-            </div>
-
-            <!-- teachers -->
-            <div class="row">
-                <div class="col-lg-12 col-sm-12 dots-wrap-content">
-                    <h2>Преподаватели и менторы</h2>
-                    <div class="dots-card">
-                        <div class="content table-responsive table-full-width">
-                            <table class="table">
-                                <tbody>
-                                <tr>
-                                    <td class="dots-tb-cont-logo"><img
-                                                src="{{ asset('frontend-bundle/media/teacher.jpg') }}"
-                                                alt="sponsor-logo"/></td>
-                                    <td class="dots-tb-cont-name">Название</td>
-                                    <td class="dots-tb-cont-description">Описание</td>
-                                </tr>
-                                <tr>
-                                    <td class="dots-tb-cont-logo"><img
-                                                src="{{ asset('frontend-bundle/media/teacher.jpg') }}"
-                                                alt="sponsor-logo"/></td>
-                                    <td class="dots-tb-cont-name">Название</td>
-                                    <td class="dots-tb-cont-description">Описание</td>
-                                </tr>
-                                <tr>
-                                    <td class="dots-tb-cont-logo"><img
-                                                src="{{ asset('frontend-bundle/media/teacher.jpg') }}"
-                                                alt="sponsor-logo"/></td>
-                                    <td class="dots-tb-cont-name">Название</td>
-                                    <td class="dots-tb-cont-description">Описание</td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
-
-                        <a href="#" class="dots-full-content-link"><i class="ti-arrow-circle-down"></i>Все
-                            преподаватели и менторы</a>
-                    </div>
-                </div>
-            </div>
-
         </div>
+
+        <!-- description -->
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">Что такое dots?</div>
+                    <div class="card-block">
+                        <p class="text-justify">
+                            <b>Dots</b> - это тестирующая система для dots.org.ua, написаная на Python, sh и C и интенсивно
+                                        использующая все возможности Docker контейнеров и Cgroup подсистемы ядра, запуская компиляторы и
+                                        тестируя пользовательские решения в отдельных Docker контейнерах. Такой подход делает DDots
+                                        максимально гибкой и модульной системой. <br> <br>
+
+                                        Один DDots обрабатывает одно решение в единицу времени и предзагружает два решения в очередь для
+                                        уменьшения задержек, связанных с сетью. Таким образом, для нагрузки нескольких ядер
+                                        рекомендуется запускать N контейнеров DDots (это делается автоматически, Makefile автоматически
+                                        определяет количество доступных ядер и использует все возможности). <br><br>
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <!-- news -->
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">News</div>
+                    <div class="card-block">
+                        <div>
+                            <p>Текст последней новости...</p>
+                        </div>
+                    </div>
+                    <hr>
+
+                    <div class="card-block">
+                        <div>
+                            <p>Текст последней новости...</p>
+                        </div>
+                    </div>
+                    <hr>
+
+                    <div class="card-block">
+                        <div>
+                            <p>Текст последней новости...</p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="card-block">
+
+                        <a href="#" class="btn btn-success pull-right">Last news</a>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+        <hr class="hidden-border">
+        <!-- news -->
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">Практикум по программированию</div>
+
+                    @foreach(\App\Subdomain::get() as $subdomain)
+                        <div class="card-block">
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <a href="{{ $subdomain->getUrl() }}"><img src="{{ $subdomain->image }}" alt="sponsor-logo" class="sponsor-logo" /></a>
+                                </div>
+                                <div class="col-sm-4">
+                                    <a href="{{ $subdomain->getUrl() }}">{{ $subdomain->title }}</a>
+                                </div>
+                                <div class="col-sm-4">
+                                    {{ $subdomain->description }}
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+                    @endforeach
+
+                    <div class="card-block">
+                        <a href="#" class="btn btn-success pull-right">All subdomains</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <hr class="hidden-border">
+        <!-- news -->
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">Project was supported by</div>
+
+                    @foreach(\App\Sponsor::main()->get() as $sponsor)
+                        <div class="card-block">
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <a href="{{ $sponsor->link }}"><img src="{{ $sponsor->image }}" alt="sponsor-logo" class="sponsor-logo" /></a>
+                                </div>
+                                <div class="col-sm-4">
+                                    <a href="{{ $sponsor->link }}">{{ $sponsor->name }}</a>
+                                </div>
+                                <div class="col-sm-4">
+                                    {{ $sponsor->description }}
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+                    @endforeach
+
+                    <div class="card-block">
+                        <a href="#" class="btn btn-success pull-right">All sponsors</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+        <hr class="hidden-border">
+        <!-- news -->
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">Subdomain's teachers and mentors</div>
+                    <div class="card-block">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <a href="#"><img src="{{ asset('frontend-bundle/media/teacher.jpg') }}" alt="sponsor-logo" class="sponsor-logo" /></a>
+                            </div>
+                            <div class="col-sm-4">
+                                <a href="#">Название</a>
+                            </div>
+                            <div class="col-sm-4">
+                                Описание
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+
+                    <div class="card-block">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <a href="#"><img src="{{ asset('frontend-bundle/media/teacher.jpg') }}" alt="sponsor-logo" class="sponsor-logo" /></a>
+                            </div>
+                            <div class="col-sm-4">
+                                <a href="#">Название</a>
+                            </div>
+                            <div class="col-sm-4">
+                                Описание
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+
+                    <div class="card-block">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <a href="#"><img src="{{ asset('frontend-bundle/media/teacher.jpg') }}" alt="sponsor-logo" class="sponsor-logo" /></a>
+                            </div>
+                            <div class="col-sm-4">
+                                <a href="#">Название</a>
+                            </div>
+                            <div class="col-sm-4">
+                                Описание
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+
+                    <div class="card-block">
+                        <a href="#" class="btn btn-success pull-right">All teachers and mentors</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
     </div>
 @endsection
