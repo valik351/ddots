8aa30fda5c95dde770961a96bec995b523f9d706
qazwsxedcrfvtgb1111 <tttdima60@gmail.com>
2016-11-01 22:29:36 +0200

    markup fix

--- resources/views/messages/list.blade.php
+++ resources/views/messages/list.blade.php
@@ -8,34 +8,37 @@
             </div>
         </div>
         <hr class="hidden-border">
-        <div class="row">
-            <div class="table-responsive">
-                @if(!$dialog_users->isEmpty())
-                    <table class="table table-striped table-bordered table-sm">
-                        <thead>
-                        <tr>
-                            <th>Username</th>
-                            <th>Last message</th>
-                            <th>Date</th>
-                        </tr>
-                        </thead>
-                        <tbody>
+        <div class="card">
+            <div class="card-header">Dialogs</div>
+            <div class="card-block">
+                <div class="table-responsive">
+                    @if(!$dialog_users->isEmpty())
+                        <table class="table table-striped table-bordered table-sm">
+                            <thead>
+                            <tr>
+                                <th>Username</th>
+                                <th>Last message</th>
+                                <th>Date</th>
+                            </tr>
+                            </thead>
+                            <tbody>
 
-                        @foreach($dialog_users as $user)
-                        <tr>
-                            <td class="no-wrap">{{ $user->hasRole(\App\User::ROLE_ADMIN)?'admin':$user->name }}</td>
-                            <td>
-                                <a class="breaking-word" href="{{ route('frontend::messages::dialog', ['id' => $user->id]) }}#send-message">
-                                    {{ $user->getLastMessageWith(Auth::user()->id)->getSenderName() }}: {{ $user->getLastMessageWith(Auth::user()->id)->text }}
-                                </a>
-                            </td>
-                            <td class="no-wrap">{{ $user->getLastMessageWith(Auth::user()->id)->created_at }}</td>
-                        </tr>
+                            @foreach($dialog_users as $user)
+                                <tr>
+                                    <td class="no-wrap">{{ $user->hasRole(\App\User::ROLE_ADMIN)?'admin':$user->name }}</td>
+                                    <td>
+                                        <a class="breaking-word" href="{{ route('frontend::messages::dialog', ['id' => $user->id]) }}#send-message">
+                                            {{ $user->getLastMessageWith(Auth::user()->id)->getSenderName() }}: {{ $user->getLastMessageWith(Auth::user()->id)->text }}
+                                        </a>
+                                    </td>
+                                    <td class="no-wrap">{{ $user->getLastMessageWith(Auth::user()->id)->created_at }}</td>
+                                </tr>
 
-                        @endforeach
-                        </tbody>
-                    </table>
-                @endif
+                            @endforeach
+                            </tbody>
+                        </table>
+                    @endif
+                </div>
             </div>
         </div>
     </div>
