05335bfa84f324621d15db69bff087581933d723
qazwsxedcrfvtgb1111 <tttdima60@gmail.com>
2016-11-01 22:42:49 +0200

    fast fix

--- resources/views/contests/standings.blade.php
+++ resources/views/contests/standings.blade.php
@@ -45,40 +45,41 @@
                         </tr>
                         </tfoot>
                         <tbody>
-                        @if(!empty($users))
-                            @foreach($users as $user_id => $user)
-                                <tr>
-                                    <td>
-                                        @if(Auth::user()->hasRole(\App\User::ROLE_TEACHER) && Auth::user()->isTeacherOf($user_id) || Auth::user()->id == $user_id)
-                                            <a href="{{ route('frontend::user::profile', ['id' => $user_id]) }}">
-                                                {{ $user['name'] }}
-                                            </a>
-                                        @else
+                        @foreach($users as $user_id => $user)
+                            <tr>
+                                <td>
+                                    @if(Auth::user()->hasRole(\App\User::ROLE_TEACHER) && Auth::user()->isTeacherOf($user_id) || Auth::user()->id == $user_id)
+                                        <a href="{{ route('frontend::user::profile', ['id' => $user_id]) }}">
                                             {{ $user['name'] }}
-                                        @endif
+                                        </a>
+                                    @else
+                                        {{ $user['name'] }}
+                                    @endif
 
-                                    </td>
-                                    @foreach($user['problems'] as $problem_id => $problem)
-                                        <td>
-                                            @if($problem['points'] && $problem['solution_id'])
-                                                @if(Auth::user()->hasRole(\App\User::ROLE_TEACHER))
-                                                    <a href="{{ route('frontend::contests::solution', ['id' => $problem['solution_id']]) }}">
-                                                        {{ $problem['points'] }}
-                                                    </a>
-                                                @else
-                                                    {{ $problem['points'] }}
-                                                @endif
-                                            @else
-                                                -
-                                            @endif
-                                        </td>
-                                    @endforeach
+                                </td>
+
+                                @if(array_key_exists('prblems', $user)){{-- @todo:refactor that shit! --}}
+                                @foreach($user['problems'] as $problem_id => $problem)
                                     <td>
-                                        {{ $user['points'] }}
+                                        @if($problem['points'] && $problem['solution_id'])
+                                            @if(Auth::user()->hasRole(\App\User::ROLE_TEACHER))
+                                                <a href="{{ route('frontend::contests::solution', ['id' => $problem['solution_id']]) }}">
+                                                    {{ $problem['points'] }}
+                                                </a>
+                                            @else
+                                                {{ $problem['points'] }}
+                                            @endif
+                                        @else
+                                            -
+                                        @endif
                                     </td>
-                                </tr>
-                            @endforeach
-                        @endif
+                                @endforeach
+                                @endif
+                                <td>
+                                    {{ $user['points'] }}
+                                </td>
+                            </tr>
+                        @endforeach
                         </tbody>
                     </table>
                 </div>
