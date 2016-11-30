ac63f9701bf45bc895a8fa373958237726622920
valik351 <av.val.100@gmail.com>
2016-11-30 17:38:08 +0200

    add solution seeder

--- database/seeds/TestSolutionsTableSeeder.php
+++ database/seeds/TestSolutionsTableSeeder.php
@@ -3,6 +3,7 @@
 use App\Contest;
 use App\Problem;
 use App\Solution;
+use Carbon\Carbon;
 use Faker\Generator;
 use Illuminate\Database\Seeder;
 
@@ -19,27 +20,26 @@ class TestSolutionsTableSeeder extends Seeder
         $contest = Contest::findOrFail(1);
         $problems = $contest->problems;
         $users = $contest->users;
+        $languages = $contest->programming_languages;
 
         $generator = Faker\Factory::create();
 
 
-        $solutions = factory(Solution::class, 2000)->make()
-            ->each(function (Solution $solution) use ($contest, $problems, $users, $generator) {
-                $solution->state = Solution::STATE_TESTED;
-                $solution->status = Solution::STATUS_CE;
-                $solution->testing_mode = 'full';
+        $solutions = factory(Solution::class, 10000)->make()
+            ->each(function (Solution $solution) use ($contest, $problems, $users, $languages, $generator) {
+                $solution->state = Solution::STATE_NEW;
+                $solution->testing_mode = collect(['full', 'first_fail', 'one'])->random(1);
                 $solution->problem_id = $problems->random(1)->id;
-                $solution->programming_language_id = 1;
-                $solution->reviewed = 1;
+
+                $solution->programming_language_id = $languages->random(1)->id;
+
                 $solution->user_id = $users->random(1)->id;
-                $solution->created_at = $generator->dateTimeBetween($contest->start_date, $contest->end_date);
-                $solution->success_percentage = rand(0, 100);
-                if($solution->success_percentage == 100) {
-                    $solution->state = Solution::STATE_TESTED;
-                    $solution->status = Solution::STATUS_OK;
-                }
+                $solution->created_at = Carbon::now();
 
                 $solution->save();
+
+                file_put_contents($solution->sourceCodeFilePath(), 'some code');
+
             });
 
 
